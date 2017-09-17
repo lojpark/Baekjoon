3ef12d16996a9ff2838867e1ee5a9292396fcf42
lojpark <lojpark@naver.com>
2017-09-17 12:43:49 +0900

    13334: Priority queue

--- 13334/13334.cpp
+++ 13334/13334.cpp
@@ -1,34 +1,67 @@
 /*
-Just coding
+Priority queue
 */
 #include <iostream>
+#include <queue>
 #include <algorithm>
 
-int n;
-int a[1010];
+using namespace std;
 
-bool cmp(int q, int qq)
-{
-	return q > qq;
+int n, d;
+struct Data{
+	int st, ed;
+}a[100010];
+
+bool cmp(Data q, Data qq) {
+	return q.ed < qq.ed;
 }
+
+struct qcmp {
+	bool operator()(Data q, Data qq) {
+		return q.st > qq.st;
+	}
+};
 
 int main()
 {
-	int i;
-	std::cin >> n;
+	int i, tmp;
+	cin >> n;
 	for (i = 1; i <= n; i++) {
-		std::cin >> a[i];
+		cin >> a[i].st >> a[i].ed;
+		if (a[i].st > a[i].ed) {
+			tmp = a[i].st;
+			a[i].st = a[i].ed;
+			a[i].ed = tmp;
+		}
+	}
+	cin >> d;
+
+	sort(a + 1, a + 1 + n, cmp);
+
+	priority_queue<Data, vector<Data>, qcmp> q;
+
+	int l = 0, cnt = 0, max = 0;
+	for (i = 1; i <= n; i++) {
+		if (a[i].ed - a[i].st > d) continue;
+
+		if (l <= a[i].st && a[i].ed <= l + d) {
+			q.push(a[i]);
+		}
+		else {
+			l = a[i].ed - d;
+			while (q.size() > 0) {
+				if (q.top().st < l)
+					q.pop();
+				else
+					break;
+			}
+			q.push(a[i]);
+		}
+		if (max < q.size())
+			max = q.size();
 	}
 
-	std::sort(a + 1, a + 1 + n, cmp);
+	cout << max;
 
-	for (i = n; i >= 1; i--) {
-		if (a[i] >= i) {
-			std::cout << i;
-			return 0;
-		}
-	}	
-
-	std::cout << "0";
 	return 0;
 }
